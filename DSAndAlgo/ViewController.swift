4b47c33d9fe0907309430282542f4fb49b925db0
Subhra Roy <subhra.roy@e-arc.com>
2019-06-03 15:19:31 +0530

    add search to tree

--- DSAndAlgo/ViewController.swift
+++ DSAndAlgo/ViewController.swift
@@ -14,21 +14,57 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let rootNode = self.createRootNode()
+        let rootNode : Node = self.createRootNode()
         let treeDescription : String = rootNode.description
         print("\(treeDescription)")
+        
+        let foundNode : Node = rootNode.search(data: "Milk")!
+        print("\(foundNode)")
+        print("\(String(describing: foundNode.parentNode?.description))")
     }
     
     private func createRootNode() -> Node<String>{
         
         let rootNode : Node<String> = Node(data: "Beverages")
-        let leftChild : Node<String> = Node(data: "Hot")
-        let rightChild : Node<String> = Node(data: "Cold")
-        rootNode.addChild(child: leftChild)
-        rootNode.addChild(child: rightChild)
+        
+        let hotBeverage = Node(data: "Hot")
+        let coldBeverage = Node(data: "Cold")
+        
+        let tea = Node(data: "Tea")
+        let coffee = Node(data: "Coffee")
+        let cocoa = Node(data: "Cocoa")
+        
+        let blackTea = Node(data: "Black")
+        let greenTea = Node(data: "Green")
+        let chaiTea = Node(data: "Chai")
+        
+        let soda = Node(data: "Soda")
+        let milk = Node(data: "Milk")
+        
+        let gingerAle = Node(data: "Ginger ale")
+        let bitterLemon = Node(data: "Bitter lemon")
+        
+        rootNode.addChild(child: hotBeverage)
+        rootNode.addChild(child: coldBeverage)
+        
+        hotBeverage.addChild(child: tea)
+        hotBeverage.addChild(child: coffee)
+        hotBeverage.addChild(child: cocoa)
+        
+        coldBeverage.addChild(child: soda)
+        coldBeverage.addChild(child: milk)
+        
+        tea.addChild(child: blackTea)
+        tea.addChild(child: greenTea)
+        tea.addChild(child: chaiTea)
+        
+        soda.addChild(child: gingerAle)
+        soda.addChild(child: bitterLemon)
+
         
         return rootNode
     }
+
 
 }
 
@@ -36,7 +72,7 @@ class Node<T>{
     
     var value : T?
     private var children : [Node] = [Node]()
-    private weak var parentNode : Node?
+    internal  private(set) weak var parentNode : Node?
     convenience init(data : T){
         self.init()
         self.value = data
@@ -50,6 +86,16 @@ class Node<T>{
         child.parentNode = self
     }
     
+    func getChildren() -> [Node]?{
+        
+        if self.children.count > 0{
+            return self.children
+        }else{
+            return nil
+        }
+        
+    }
+    
 }
 
 extension Node : CustomStringConvertible{
@@ -58,7 +104,7 @@ extension Node : CustomStringConvertible{
         
          var des : String = self.value as! String
         if !self.children.isEmpty{
-            des += "{" + self.children.map{ $0.description }.joined(separator: ", ") + "}"
+            des += " { " + self.children.map{ $0.description }.joined(separator: ", ") + " } "
         }
         
         return des
