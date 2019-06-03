3ffc6c11bb588fc8a07528d27a3661b0949d6db9
Subhra Roy <subhra.roy@e-arc.com>
2019-06-03 14:05:11 +0530

    Add Simple Tree Function

--- DSAndAlgo/ViewController.swift
+++ DSAndAlgo/ViewController.swift
@@ -15,6 +15,59 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+class Node<T>{
+    
+    var value : T?
+    private var children : [Node] = [Node]()
+    private weak var parentNode : Node?
+    convenience init(data : T){
+        self.init()
+        self.value = data
+    }
+    private init(){
+        
+    }
+    
+    func addChild(child : Node){
+        self.children.append(child)
+        child.parentNode = self
+    }
+    
+}
+
+extension Node : CustomStringConvertible{
+    
+    var description: String {
+        
+         var des : String = ""
+        if !self.children.isEmpty{
+            
+            des += "{" + self.children.map{ $0.description }.joined(separator: ", ") + "}"
+        }
+        
+        return des
+    }
+    
+}
+
+extension Node where T : Equatable{
+    
+    func search(data : T) -> Node?{
+        
+        if self.value == data{
+            return self
+        }else{
+            
+            for (_ , child) in self.children.enumerated(){
+                
+                if let nodeFound : Node = child.search(data: data){
+                    return nodeFound
+                }
+            }
+        }
+        return nil
+    }
+    
+}
